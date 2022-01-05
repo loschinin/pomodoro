7455bf242fd8dba171987dead5177459296b61d0
dmitryloschinin <loschinin@list.ru>
2022-01-05 17:51:28 +0300

    start, wip...

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                Reddit for our own
+            </h1>
+            
+        </div>
+    );
+};
+
+export default Header;
