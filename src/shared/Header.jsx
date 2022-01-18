8c10ef7181208f1e588d6ef2857090169d237d1b
dmitryloschinin <loschinin@list.ru>
2022-01-18 21:27:14 +0300

    3.8 Домашняя работа

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {hot} from 'react-hot-loader/root';
 import styles from './header.less';
+import {concat} from "../example";
 
 const _Header = () => {
+    const result = concat('Hello ', 'World') // -> Hello World;
+    console.log(result)
     return (
         <div className={styles.example}>
                 Hello React
